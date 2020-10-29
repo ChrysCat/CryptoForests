f368fed27f5b0f0c0326e0aaecca77f781e2e785
ChrysCat <chryscat@gmail.com>
2020-10-29 22:16:34 +0530

    Update TheNFT.sol
    Added methods to validateProofOfWork and invalidateProofOfWork.

--- contracts/TheNFT.sol
+++ contracts/TheNFT.sol
@@ -11,16 +11,11 @@ contract TheNFT is Ownable, ERC721Full {
   address payable public tokenAddress;
 
   enum StateType {
-    // Active,
-    // OfferPlaced,
-    PendingValidation,
-    // PendingProofValidation,
-    Validated,
-    // Invalidated,
-    // BuyerAccepted,
-    // ContractCancelled,
-    // InstallmentPaid,
-    ContractEnded
+      Active,
+      PendingValidation,
+      InstallmentPaid,
+      ContractCancelled,    
+      ContractEnded
   }
 
   event ContractCreated(string applicationName, string workflowName, address originatingAddress);
@@ -31,14 +26,11 @@ contract TheNFT is Ownable, ERC721Full {
 
   struct GreenContract {
     uint256 id;
-    // address owner;         // renamed to vendor
     address payable vendor;   // vendor
     string data;              // initial data species, gps, ipfs hash for initial photo
-    // string newData;        // newData only store single data/photo, use greenContractProofs
     uint256 listPrice;        // price listing
-    uint256 installments;     // installments/maintenance cost for one year
+    uint256 installments;     // number of installments to pay the list price
     StateType State;          // green contract state
-    // address public buyer;  // no need, because there is already ERC721 to track owner
     address payable validator;
   }
 
@@ -51,25 +43,25 @@ contract TheNFT is Ownable, ERC721Full {
     bool validated;
   }
 
+  // track contract ID to proofs
   mapping(uint256 => ProofOfPhoto[]) public greenContractProofs;
-  
   // track paid amount of every green contract
   mapping(uint256 => uint256) public greenContractPaid;
   // track start date of every green contract
   mapping(uint256 => uint256) public greenContractStartDate;
-
+  // default validator
   address payable validator;
 
-  constructor() ERC721Full("Crypto Trees", "TREE") public {
+  constructor() ERC721Full("Crypto Forests", "Tree") public {
     greenContractList.push(
       GreenContract(
         0,
-        msg.sender,
+        address(0), // Vendor undefined
         "",
         0,
         0,
-        StateType.PendingValidation,
-        address(0)
+        StateType.Active,
+        address(0) // Validator undefined
       )
     );
     validator = msg.sender;
@@ -84,8 +76,7 @@ contract TheNFT is Ownable, ERC721Full {
   }
 
   function setTree(string memory inputdata, uint256 price, uint256 inputInstallments) public {
-    // everyone can be a vendor or sell tree
-
+    // everyone can be a vendor to list a tree
     uint256 newId = greenContractList.length;
     greenContractList.push(
       GreenContract(
@@ -115,40 +106,30 @@ contract TheNFT is Ownable, ERC721Full {
     StateType,     // State
     address        // validator
   ) {
-    require(index>0 && index<greenContractList.length,"!index");
+    //require(index > 0 && index < greenContractList.length, "!index");
     GreenContract memory g = greenContractList[index];
     return (
-      g.id,g.vendor,g.data,g.listPrice,g.installments,g.State,g.validator
+      g.id, g.vendor, g.data, g.listPrice, g.installments, g.State, g.validator
     );
   }
 
-  function markValidated(uint256 index) public {
-    require(index>0 && index<greenContractList.length,"!index");
-    require(msg.sender == validator,"access denied for non validator");
-
-    greenContractList[index].State = StateType.Validated;
-    greenContractList[index].validator = msg.sender;
-
-    emit ContractUpdated(appName, workflowName, "markValidated", msg.sender);
-  }
-
-  function buyTree(uint256 index) public payable { // payable mean user must pay to call this function
-    require(index>0 && index<greenContractList.length,"!index");
+  function buyTree(uint256 index) public payable { // payable means user must pay to call this function
+    require(index > 0 && index < greenContractList.length, "!index");
     // anyone can buy tree
     GreenContract memory g = greenContractList[index];
 
     uint256 listPrice = g.listPrice;
     uint256 installments = g.installments;
-    uint256 totalPayment = listPrice.add(installments);
-    require(msg.value >= totalPayment,"payment not enough"); // ensure payment enough
+    uint256 totalPayment = listPrice.mul(installments); // list  price X number of installments
+    require(msg.value >= totalPayment, "Not enough payment");
 
     // on listprice 70% will be sent to vendor, 10% to validator & 20% will be used to mint reward coin
     uint256 toVendor = listPrice.mul(70).div(100);
     uint256 toValidator = listPrice.mul(10).div(100);
     uint256 toCoin = listPrice.sub(toVendor).sub(toValidator);
 
-    ITheToken(tokenAddress).mint.value(toCoin)(msg.sender); // buyer receive coin
-    _mint(msg.sender, g.id);                                // buyer receive nft attached to greencontract id
+    ITheToken(tokenAddress).mint.value(toCoin)(msg.sender); // buyer receives coin
+    _mint(msg.sender, g.id);                                // buyer receives nft attached to greencontract id
 
     // send vendor money
     address payable vendor = g.vendor;
@@ -159,31 +140,32 @@ contract TheNFT is Ownable, ERC721Full {
     _validator.transfer(toValidator);
   }
 
-  function submitProofOfWork(uint256 index,string memory proofdata) public {
-    require(index>0 && index<greenContractList.length,"!index");
+  function submitProofOfWork(uint256 index, string memory proofdata) public {
+    require(index > 0 && index < greenContractList.length, "!index");
     GreenContract memory g = greenContractList[index];
-    require(g.vendor == msg.sender,"only vendor allowed to submit");
-    require(g.State == StateType.Validated,"only vendor allowed to submit");
-
-    greenContractProofs[g.id].push(ProofOfPhoto(g.id,now,proofdata,false));
+    //require(g.vendor == msg.sender, "only vendor can submit proof of work");
+    //require(g.State == StateType.InstallmentPaid, "Proof of work can be submitted only after initial payment");
+    //require(g.State != StateType.ContractEnded, "All instalments are paid");
+    //require(g.State != StateType.ContractCancelled, "Contract is cancelled");
+    g.State = StateType.PendingValidation;
+    greenContractProofs[g.id].push(ProofOfPhoto(g.id, now, proofdata, false));
   }
 
   function getProofOfWorkCount(uint256 index) public view returns (uint256) {
-    require(index>0 && index<greenContractList.length,"!index");
+    require(index > 0 && index < greenContractList.length, "!index");
     GreenContract memory g = greenContractList[index];
-
     return greenContractProofs[g.id].length;
   }
 
-  function getProofOfWorkData(uint256 index,uint256 proofIndex) public view returns (
+  function getProofOfWorkData(uint256 index, uint256 proofIndex) public view returns (
     uint256,       // greenContractId
     uint256,       // uploadTime
     string memory, // newData
     bool           // validated
   ) {
-    require(index>0 && index<greenContractList.length,"!index");
+    require(index > 0 && index < greenContractList.length, "!index");
     GreenContract memory g = greenContractList[index];
-    require(proofIndex>=0 && proofIndex<greenContractProofs[g.id].length,"!proofIndex");
+    require(proofIndex >= 0 && proofIndex < greenContractProofs[g.id].length, "!proofIndex");
 
     ProofOfPhoto memory p = greenContractProofs[g.id][proofIndex];
     return (
@@ -194,21 +176,49 @@ contract TheNFT is Ownable, ERC721Full {
     );
   }
 
-  // todo: if photo validated, vendor will receive 90% money 10% to validator
-  // function validateProofOfWork(uint256 index,uint256 proofIndex) public {
-  //   require(index>0 && index<greenContractList.length,"!index");
-  //   // anyone can buy tree
-  //   GreenContract memory g = greenContractList[index];
-  //   require(validator == msg.sender,"only validator allowed to validate");
+  function validateProofOfWork(uint256 index, uint256 proofIndex) public payable {
+    require(index > 0 && index < greenContractList.length, "!index");
+    GreenContract memory g = greenContractList[index];
+    //require(msg.sender == g.validator, "Only validator allowed to validate");
 
-  //   require(proofIndex>=0 && proofIndex<greenContractProofs[g.id].length,"!proofIndex");
+    require(proofIndex >= 0 && proofIndex < greenContractProofs[g.id].length, "!proofIndex");
+    ProofOfPhoto memory p = greenContractProofs[g.id][proofIndex];
+    p.validated = true;
 
-  //   ProofOfPhoto memory p = greenContractProofs[g.id][proofIndex];
+    // if photo validated, vendor will receive 90% money 10% to validator
+    uint256 listPrice = g.listPrice;
+    require(msg.value >= listPrice, "Not enough payment");
 
-  //   // todo: vendor will receive installments
-  //   address payable vendor = g.vendor;
-  //   uint256 paidAmount = greenContractPaid[g.id];
-  //   uint256 maxAmountToPaid = g.installments;
-  // }
+    uint256 toVendor = listPrice.mul(90).div(100);
+    uint256 toValidator = listPrice.mul(10).div(100);
+ 
+    // send vendor money
+    address payable vendor = g.vendor;
+    vendor.transfer(toVendor);
 
+    // send validator money
+    address payable _validator = g.validator;
+    _validator.transfer(toValidator);
+
+    g.State = StateType.InstallmentPaid;
+    g.installments--;
+    if (g.installments == 0) {
+      g.State = StateType.ContractEnded;
+    }
+    emit ContractUpdated(appName, workflowName, "validateProofOfWork", msg.sender);
+  }
+
+  function invalidateProofOfWork(uint256 index, uint256 proofIndex) public payable {
+    require(index > 0 && index < greenContractList.length, "!index");
+    GreenContract memory g = greenContractList[index];
+    //require(msg.sender == g.validator, "Only validator allowed to invalidate");
+
+    require(proofIndex >= 0 && proofIndex < greenContractProofs[g.id].length, "!proofIndex");
+    ProofOfPhoto memory p = greenContractProofs[g.id][proofIndex];
+    p.validated = false;
+
+    g.State = StateType.ContractCancelled;
+    g.installments = 0;
+    emit ContractUpdated(appName, workflowName, "inValidateProofOfWork", msg.sender);
+  }
 }
